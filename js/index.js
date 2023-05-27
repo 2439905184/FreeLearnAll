1af44623ffa3b83e2213a5b16a61294cbe008192
13023335265 <suchenglong018@126.com>
2023-05-27 18:56:29 +0800

    🦄 refactor: 重构主页和样式

---
+++ js/index.js
@@ -0,0 +1,12 @@
+console.log("网站内有乾坤彩蛋")
+console.log("阳数")
+console.log("点击次数")
+var times = 0
+function jumpInner()
+{
+    times += 1
+    if(times == 5)
+    {
+        window.open("010 易校/首页.html")
+    }
+}
